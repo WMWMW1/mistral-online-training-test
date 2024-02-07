c04c2e49983ac66af6bbc307c3c3ccb2098910a5
Your GitHub Username <129616906+WMWMW1@users.noreply.github.com>
2024-02-07 17:11:27 +0000

    Update qlora_.py

--- qlora_.py
+++ qlora_.py
@@ -1,7 +1,26 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from peft import LoraConfig,get_peft_model
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from peft import LoraConfig, get_peft_model
+
+# PEFT configuration
 peft_config = LoraConfig(
-     inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+    inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
 )
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
-model = get_peft_model(model, peft_config)
+
+# Load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1",load_in_4bit=True)
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+# Set tokenizer padding token
+tokenizer.pad_token = tokenizer.eos_token
+
+# Adapt model with PEFT
+# model = get_peft_model(model, peft_config)
+
+# Move model to GPU
+model = model.to("cuda")  # This line moves the model to the GPU
+
+# Tokenize inputs and move them to the same device as the model
+
+
+pipeline = pipeline("text-generation",tokenizer = tokenizer, model=model, device=0)
+
